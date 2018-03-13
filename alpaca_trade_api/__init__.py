ffe0e1907a990fc3742519c5b15f4f3a0a111bec
Hitoshi Harada <hitoshi@alpacadb.com>
2018-03-12 23:38:34 -0700

    Add setup.py

--- alpaca_trade_api/__init__.py
+++ alpaca_trade_api/__init__.py
@@ -1,2 +1,4 @@
 from .rest import REST  # noqa
 from .stream import StreamConn  # noqa
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import ast
+import re
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('alpaca_trade_api/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='alpaca-trade-api',
+    version=version,
+    description='Alpaca API python client',
+    author='Alpaca',
+    author_email='oss@alpaca.markets',
+    url='https://github.com/alpacahq/alpaca-trade-api-python',
+    keywords='financial,timeseries,api,trade',
+    packages=['alpaca_trade_api', ],
+    install_requires=[
+        'python-dateutil',
+        'requests',
+        'urllib3',
+        'websocket-client',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'requests-mock',
+        'coverage>=4.4.1',
+        'mock>=1.0.1'
+    ],
+    setup_requires=['pytest-runner', 'flake8'],
+)

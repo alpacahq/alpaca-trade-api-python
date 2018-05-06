fa1cb1d0a49a48c30fe56eae9b76fdcddb570aa8
Hitoshi Harada <hitoshi@alpacadb.com>
2018-05-05 23:44:04 -0700

    Add main

---
+++ alpaca_trade_api/__main__.py
@@ -0,0 +1,26 @@
+import argparse
+from .rest import REST
+
+
+def run(args):
+    api = REST(**args)
+    try:
+        from IPython import embed
+        embed()
+    except ImportError:
+        import code
+        code.interact(locals=locals())
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key-id', help='APCA_API_KEY_ID')
+    parser.add_argument('--secret-key', help='APCA_API_SECRET_KEY')
+    parser.add_argument('--base-url')
+    args = parser.parse_args()
+
+    run({k: v for k, v in vars(args).items() if v is not None})
+
+
+if __name__ == '__main__':
+    main()

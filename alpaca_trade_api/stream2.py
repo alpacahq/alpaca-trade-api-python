4b8e6866ea187972b71643095cefb69257089edb
ttt733 <trevor.thackston@gmail.com>
2018-12-24 16:44:50 -0600

    Change 'authenticated' to 'authorized' in stream2

--- alpaca_trade_api/stream2.py
+++ alpaca_trade_api/stream2.py
@@ -31,14 +31,14 @@ class StreamConn(object):
             r = r.decode('utf-8')
         msg = json.loads(r)
 
-        if 'data' not in msg or msg['data']['status'] != 'authorized':
+        if not 'data' in msg or msg['data']['status'] != 'authorized':
             raise ValueError(
                 ("Invalid Alpaca API credentials, Failed to authenticate: {}"
                     .format(msg))
             )
 
         self._ws = ws
-        await self._dispatch('authenticated', msg)
+        await self._dispatch('authorized', msg)
 
         asyncio.ensure_future(self._consume_msg())
         return ws

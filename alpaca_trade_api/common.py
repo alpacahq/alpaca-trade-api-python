a20bb803695ba27e94b7d209701c602703e0c540
Brian J. Dowling <bjd-dev@simplicity.net>
2019-07-15 14:13:34 -0400

    Improved messages on missing key to mention env

--- alpaca_trade_api/common.py
+++ alpaca_trade_api/common.py
@@ -14,23 +14,26 @@ def get_data_url():
 def get_credentials(key_id=None, secret_key=None):
     key_id = key_id or os.environ.get('APCA_API_KEY_ID')
     if key_id is None:
-        raise ValueError('Key ID must be given to access Alpaca trade API')
+        raise ValueError('Key ID must be given to access Alpaca trade API',
+                         '(env: APCA_API_KEY_ID)')
 
     secret_key = secret_key or os.environ.get('APCA_API_SECRET_KEY')
     if secret_key is None:
-        raise ValueError('Secret key must be given to access Alpaca trade API')
+        raise ValueError('Secret key must be given to access Alpaca trade API'
+                         ' (env: APCA_API_SECRET_KEY')
 
     return key_id, secret_key
 
 
 def get_polygon_credentials(alpaca_key=None):
     try:
-        alpaca_key, _ = get_credentials(alpaca_key)
+        alpaca_key, _ = get_credentials(alpaca_key, 'ignored')
     except ValueError:
         pass
     key_id = os.environ.get('POLYGON_KEY_ID') or alpaca_key
     if key_id is None:
-        raise ValueError('Key ID must be given to access Polygon API')
+        raise ValueError('Key ID must be given to access Polygon API'
+                         '(env: APCA_API_KEY_ID or POLYGON_KEY_ID)')
     return key_id
 
 

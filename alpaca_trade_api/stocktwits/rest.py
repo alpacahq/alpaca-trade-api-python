a64b58527233df365800b8bcc803ce69c7bc2d33
Ben <beale.ben@gmail.com>
2020-03-21 11:22:26 -0400

    WIP: Stocktwits integration - first commit.

---
+++ alpaca_trade_api/stocktwits/rest.py
@@ -0,0 +1,18 @@
+
+
+class REST(object):
+
+    def __init__(self, api_key):
+        """
+        TODO: Stocktwits!
+
+        https://api.stocktwits.com/developers/docs/api
+        """
+        pass
+
+    def _request(self, method, params=None):
+        pass
+
+    def get(self, params=None):
+        pass
+

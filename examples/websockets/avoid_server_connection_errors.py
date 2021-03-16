895a5cd09ad43b76e1667bb1d15512f8cdd72395
camelpac <camel.pac@gmail.com>
2021-03-16 18:37:40 +0200

    server connection protection example - update to data v2

--- examples/websockets/avoid_server_connection_errors.py
+++ examples/websockets/avoid_server_connection_errors.py
@@ -5,51 +5,37 @@ in case of ws disconnection.
 
 import logging
 import time
-from alpaca_trade_api import StreamConn
+from alpaca_trade_api.stream import Stream
 from alpaca_trade_api.common import URL
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 ALPACA_API_KEY = "<YOUR-API-KEY>"
 ALPACA_SECRET_KEY = "<YOUR-SECRET-KEY>"
-USE_POLYGON = False
 
 
-def run_connection(conn, channels):
+def run_connection(conn):
     try:
-        conn.run(channels)
+        conn.run()
     except Exception as e:
         print(f'Exception from websocket connection: {e}')
     finally:
         print("Trying to re-establish connection")
         time.sleep(3)
-        run_connection(conn, channels)
+        run_connection(conn)
+
+
+async def print_quote(q):
+    print('quote', q)
+
 
 if __name__ == '__main__':
-    channels = ['alpacadatav1/Q.GOOG']
+    conn = Stream(ALPACA_API_KEY,
+                  ALPACA_SECRET_KEY,
+                  base_url=URL('https://paper-api.alpaca.markets'),
+                  data_feed='iex')
 
-    conn = StreamConn(
-        ALPACA_API_KEY,
-        ALPACA_SECRET_KEY,
-        base_url=URL('https://paper-api.alpaca.markets'),
-        data_url=URL('https://data.alpaca.markets'),
-        # data_url=URL('http://127.0.0.1:8765'),
-        data_stream='polygon' if USE_POLYGON else 'alpacadatav1'
-    )
+    conn.subscribe_quotes(print_quote, 'AAPL')
+    conn.run()
 
-
-    @conn.on(r'^AM\..+$')
-    async def on_minute_bars(conn, channel, bar):
-        print('bars', bar)
-
-
-    @conn.on(r'Q\..+')
-    async def on_quotes(conn, channel, quote):
-        print('quote', quote)
-
-
-    @conn.on(r'T\..+')
-    async def on_trades(conn, channel, trade):
-        print('trade', trade)
-
-    run_connection(conn, channels)
+    run_connection(conn)

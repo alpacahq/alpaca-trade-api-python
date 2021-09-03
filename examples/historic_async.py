bf3710b0b020e6623395515f331c32bf3fc61d4f
camelpac <camel.pac@gmail.com>
2021-09-03 19:16:44 +0300

    only iterate active symbols

--- examples/historic_async.py
+++ examples/historic_async.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
 
     start_time = time.time()
     loop = asyncio.get_event_loop()
-    symbols = [el.symbol for el in api.list_assets()]
+    symbols = [el.symbol for el in api.list_assets(status='active')]
     symbols = symbols[:200]
     loop.run_until_complete(main(symbols))
     print(f"took {time.time() - start_time} sec")

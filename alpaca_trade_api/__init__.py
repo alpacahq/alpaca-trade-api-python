f5807378dd7c7e875d605d55765e311f02723cdd
Rahul <rahulchowdh97@gmail.com>
2021-12-10 08:22:37 -0500

    Adds User Agent (#540)
    * feature/adds-user-agent
    
    * fix circular import

--- alpaca_trade_api/__init__.py
+++ alpaca_trade_api/__init__.py
@@ -1,6 +1,6 @@
+__version__ = '1.4.2'
+
 from .rest import REST, TimeFrame, TimeFrameUnit  # noqa
 from .rest_async import AsyncRest  # noqa
 from .stream import Stream  # noqa
 from .stream2 import StreamConn  # noqa
-
-__version__ = '1.4.2'
